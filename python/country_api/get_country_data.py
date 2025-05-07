c3fe72b51c994cd0e9e20861ff2d0038ca894942
S0l0m4n <sam@stickerbrush>
2025-05-07 21:05:06 +0300

    get_country_data: Write cities for each country to a separate file

--- python/country_api/get_country_data.py
+++ python/country_api/get_country_data.py
@@ -1,6 +1,7 @@
 #!/bin/bash/python3
 
 import json
+import os
 import requests
 
 API_KEY = 'c2hmYVY5bkpDbXNrWVpWRlUyelZGeU9tRTJ1Y050cDN1NTZhcVJ5Ug=='
@@ -52,13 +53,40 @@ def generate_all_city_lists(countries):
     return country_data
 
 COUNTRY_DATA_FILE = "country_data.json"
+COUNTRY_DATA_DIR = "data"
 
 if __name__ == '__main__':
-#   countries = ['MU', 'UA']
-    countries = [ item['iso2'] for item in get_countries() ]
-    country_data = generate_all_city_lists(countries)
-    with open(COUNTRY_DATA_FILE, 'w') as f:
-        for key in country_data.keys():
-            f.write("{{'{key}': ".format(key=key))
-            json.dump(country_data[key], f)
-            f.write("}\n")
+    assert os.path.isdir(COUNTRY_DATA_DIR), \
+            "ERROR: Directory `{}` does not exist!".format(COUNTRY_DATA_DIR)
+    countries = get_countries()
+
+    codes = ['MU', 'UA']
+#   codes = countries.keys()
+
+    # write each country's cities to a unique file
+    print("Generating city list for each country...")
+    for code in codes:
+        # get the country name, to append to each line
+        country = \
+            [ item['name'] for item in countries if item['iso2'] == code ].pop()
+
+        # get all the cities first, grouped by state
+        print("Handling {}...".format(country))
+        cities = get_all_cities_for_country(code)
+
+        # write cities in the format:
+        #   city, state, country
+        print("Writing out cities for {}...".format(country))
+        with open("{}/{}.txt".format(COUNTRY_DATA_DIR, code), 'w') as f:
+            # write the country alone, first
+            f.write(country + "\n")
+            for state in cities.keys():
+                # next, write the state
+                f.write(state + ", " + country + "\n" )
+                if len(cities[state]) < 2:
+                    # no city, so don't write anything more
+                    pass
+                else:
+                    for city in cities[state]:
+                        # finally, write each city and state (and country)
+                        f.write(city + ", " + state + ", " + country + "\n")
